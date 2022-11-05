8408be285e422a92fce2fd420250f3f713751367
gsagirlar <engineeringlead@protonmail.com>
2022-11-05 14:51:31 +0000

    chore: update configs and data handling

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
-use tun_tap;
 use std::io;
+use tun_tap;
 
- fn main() -> std::io::Result<()> {
+fn main() -> std::io::Result<()> {
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun).expect("failed to CI");
     //set the buffer to receive
     let mut buf = [0u8; 1504];
-    let nic_bytes = nic.recv(&mut buf)?;
-    eprintln!("read {} bytes {:?}", nic_bytes, &buf[..nic_bytes]);
+    loop {
+        let nic_bytes = nic.recv(&mut buf)?;
+        let flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+        eprintln!(
+            "read {} bytes *(flags: {:x}, proto: {:x}),  data: {:x?}",
+            nic_bytes - 4,
+            flags,
+            proto,
+            &buf[4..nic_bytes]
+        );
+    }
+
     Ok(())
-
 }
-
