be50b46af50dedf88a3c0a8e0aa20ba901cb9815
gsagirlar <engineeringlead@protonmail.com>
2022-11-05 14:09:30 +0000

    chore: update script and Cargo.toml

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cargo b --release
-sudo setcap cap_net_admin=eip $CARGO_TARGET_DIR/release/tcp
-$CARGO_TARGET_DIR/release/tcp &
+sudo setcap cap_net_admin=eip target/release/tcp
+target/release/tcp &
 pid=$!
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
