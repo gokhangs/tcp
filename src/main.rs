490d2c95c02b0d2ec16a0710ad977f7ccd6e4f56
gsagirlar <engineeringlead@protonmail.com>
2022-11-05 10:51:45 +0300

    feat (WIP): initial attempt at tun-tap

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
- 
- fn main() {
+use tun_tap;
+use std::io;
 
- }
+ fn main() -> std::io::Result<()> {
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun).expect("failed to CI");
+    //set the buffer to receive
+    let mut buf = [0u8; 1504];
+    let nic_bytes = nic.recv(&mut buf)?;
+    eprintln!("read {} bytes {:?}", nic_bytes, &buf[..nic_bytes]);
+    Ok(())
+}
 
